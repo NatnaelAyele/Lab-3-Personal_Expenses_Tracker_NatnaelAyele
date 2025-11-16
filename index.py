26aa8302a6fa7b1d15b43a1c514e94bad2e92c12
NatnaelAyele <n.eticha@alustudent.com>
2025-11-16 20:39:03 +0200

    Added the 'add expense' feature. now can insert expense and save them in a .txt file with the date accepted from the user

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+from datetime import datetime
+import os
+
+
 def menu():
     while True:
         print("="*8, "Welcome to personal expense tracker", "="*8 )
@@ -13,7 +17,7 @@ def menu():
             case '1':
                 check_balance()
             case '2':
-                ...
+                add_expense()
             case '3':
                ...
             case _:
@@ -48,7 +52,7 @@ def  add_balance():
                     print("amount must be greater than zero")
                 else:
                     return amount
-            except:
+            except ValueError:
                 print("Amount must be a number")
         else:
             print("Amount can't be empty")
@@ -66,5 +70,124 @@ def update_balance(amount):
 
     print("Amount added to balance successfuly!")
     print(f"New balance is {amount + available_balance}")
+
+
+def add_expense():
+    with open('balance.txt') as f:
+        lines = f.readlines()
+    current_balance = float(lines[0].split('=')[1].strip())
+    current_expense = float(lines[1].split('=')[1].strip())
+    print(f"Available balance is {current_balance}")
+    date = get_valid_date()
+    item_list = get_item_price()
+    print("="*9, "this are the inforation entered", "="*9)
+    sum = 0
+    for key in item_list:
+        print(f"{key}: {item_list[key]}")
+        sum += item_list[key]
+    while True:
+        confirmation = input("Is the above information correct (y/n): ").lower()
+        if confirmation:
+            match confirmation:
+                case 'y':
+                    if sum < current_balance:
+                        filename = f"expenses_{date}.txt"
+
+                        if os.path.exists(filename):
+                            with open(filename, "r") as f:
+                                lines = f.readlines()
+                                expense_id = len(lines) + 1
+                        else:
+                            expense_id = 1
+
+
+                        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        
+                        with open(filename, "a") as f:
+                            for key in item_list:
+                                f.write(f"{expense_id}. {timestamp} | {key} | {item_list[key]}\n")
+                                expense_id += 1
+
+                        
+                        new_balance = current_balance - sum
+                        with open('balance.txt', 'r') as f:
+                            lines = f.readlines()
+                        lines[0] =f"balance = {new_balance}\n"
+                        lines[1] =f"Total expenses to date = {current_expense + sum}\n"
+                        lines[2] =f"Available balance = {new_balance}\n"
+                        with open('balance.txt', 'w') as f:
+                            f.writelines(lines)
+
+                        print("Expenses saved successfully!")
+                        print(f"New available balance: {new_balance}")
+                        break
+                    else:
+                        print("insufficient balance! can not save expense")
+                        break
+                case 'n':
+                    break
+        else:
+            print("invalid input")
+
+
+
+def get_valid_date():
+    while True:
+        date_input = input("Enter date (YYYY-MM-DD): ")
+
+        try:
+            valid_date = datetime.strptime(date_input, "%Y-%m-%d")
+            return valid_date.date()
+        except ValueError:
+            print("Invalid input. Please use YYYY-MM-DD and check it's a real date.")
+
+
+def get_item_price():
+    items = {}
+
+    while True:
+        i = input("How many items do you want to insert: ")
+        if not i.strip():
+            print("Invalid input")
+            continue
+
+        try:
+            i = int(i)
+            if i <= 0:
+                print("Number must be greater than zero")
+                continue
+            break
+        except:
+            print("Must be a number!!!!")
+
+
+    for _ in range(i):
+
+        while True:
+            item = input("Enter the name of the item: ").strip()
+            if item:
+                break
+            print("Invalid item name")
+
+
+        while True:
+            price = input(f"Enter the price for {item}: ").strip()
+            if not price:
+                print("Invalid price")
+                continue
+            try:
+                price = float(price)
+                if price <= 0:
+                    print("Price cannot be less than or equal to zero")
+                    continue
+                break
+            except:
+                print("Price must be a number")
+
+        items[item] = price
+
+    return items
+
 
 menu()
